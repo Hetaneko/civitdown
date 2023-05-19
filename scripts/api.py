4669d6bfb8a0c91cff43b96748963220b6f128a0
Hetaneko <54069666+Hetaneko@users.noreply.github.com>
2023-05-19 11:16:17 +0300

    Update api.py

--- scripts/api.py
+++ scripts/api.py
@@ -15,19 +15,24 @@ def civitdown_api(_: gr.Blocks, app: FastAPI):
     @app.post("/civitdown/download")
     async def civitdown(
         query: str = Body("query", title='Model name'),
-        mtype: str = Body("none", title='Model type')
+        mtype: str = Body("none", title='Model type'),
+        link: str = Body("none", title='Model link'),
+        linkname: str = Body("none", title='Model link name')
     ):
-        thelink = "https://civitai.com/api/v1/models?query=" + query
-        r1 = requests.get(url = thelink)
-        response = r1.json()
-        if not response["items"]:
-            return "nope"
-        thelink2 = "https://civitai.com/api/v1/models/" + str(response["items"][0]["id"])
-        r2 = requests.get(url = thelink2)
-        response2 = r2.json()
-        durl = response2["modelVersions"][0]["files"][0]["downloadUrl"]
-        dname = response2["modelVersions"][0]["files"][0]["name"]
-        fulltext = "wget "+durl+" -O "+dname
+        if link == "none":
+            thelink = "https://civitai.com/api/v1/models?query=" + query
+            r1 = requests.get(url = thelink)
+            response = r1.json()
+            if not response["items"]:
+                return "nope"
+            thelink2 = "https://civitai.com/api/v1/models/" + str(response["items"][0]["id"])
+            r2 = requests.get(url = thelink2)
+            response2 = r2.json()
+            durl = response2["modelVersions"][0]["files"][0]["downloadUrl"]
+            dname = response2["modelVersions"][0]["files"][0]["name"]
+            fulltext = "wget "+durl+" -O "+dname
+        else:
+            fulltext = "wget "+link+" -O "+linkname
         if mtype == "model":
             os.chdir("/content/automatic/models/Stable-diffusion")
         elif mtype == "lora":
@@ -35,7 +40,7 @@ def civitdown_api(_: gr.Blocks, app: FastAPI):
         env = os.environ.copy()
         subprocess.run(fulltext, shell=True, env=env)
         modules.shared.refresh_checkpoints()
-        return {"name": dname, "url": durl}
+        return "Success"
     @app.post("/civitdown/removefile")
     async def removefile(
         name: str = Body("query", title='Model name'),
